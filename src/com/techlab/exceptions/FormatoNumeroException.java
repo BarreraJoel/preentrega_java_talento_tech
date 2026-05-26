a93bc18d2f23289318f6c26025a36cb628782689
BarreraJoel <joelmanuelbar@gmail.com>
2026-05-26 03:39:50 -0300

    fix: remove exception

--- src/com/techlab/exceptions/FormatoNumeroException.java
+++
@@ -1,7 +0,0 @@
-package com.techlab.exceptions;
-
-public class FormatoNumeroException extends RuntimeException {
-    public FormatoNumeroException(String message) {
-        super(message);
-    }
-}
